856f38537969c0a1c8aaae7bce36358dee9a6aed
Rashmi Subhash <rashmi.bsubash@gmail.com>
2024-11-20 19:54:35 -0500

    Added vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,9 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   build: {
+    rollupOptions: {
+      input: './public/index.html', // Path to your index.html
+    },
     outDir: '../server/public',
     emptyOutDir: true
   },
